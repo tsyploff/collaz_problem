08b863fa8a19ebaca6f51c1a975747daea23d395
Алексей <43031739+tsyploff@users.noreply.github.com>
2018-11-30 16:50:24 +0300

    Add files via upload

--- task.py
+++ task.py
@@ -2,17 +2,23 @@
 
 def collaz_sequence_len(n):
     '''Вычисляет длину сиракузской последовательности
+
     >>> collaz_sequence_len(3)
     8
     >>> collaz_sequence_len(100)
     26
+    
     '''
 
 def longest_by_collaz_sequence(n):
     '''Ищет число с самой длинной сиракузской последовательностью
+
     >>> longest_by_collaz_sequence(1000)
     871
     >>> longest_by_collaz_sequence(10000)
     6171
+
     '''
 
+import doctest
+doctest.testmod()
